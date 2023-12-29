6ce5338616bad7bb4412912a4c61f72b112a5ea1
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2023-12-30 01:30:18 +0530

    java  commit

---
+++ hello.java
@@ -0,0 +1,5 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("my name karan kumar chaudhary  my best friend is amin  ansari  and   both are full stack developer ");
+    }
+}
